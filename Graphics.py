be4c2762a44e738de3be1cdf645b6efa13d3ddfe
Sierra-Bakery <150887228+Sierra-Bakery@users.noreply.github.com>
2023-12-01 16:24:55 +0800

    Added Loading screen

--- Graphics.py
+++ Graphics.py
@@ -8,8 +8,19 @@ width = 530
 x = (window.winfo_screenwidth()//2)-(width//2)
 y = (window.winfo_screenheight()//2)-(height//2)
 window.geometry('{}x{}+{}+{}'.format(width, height, x, y))
-window.overrideredirect(TRUE)
+#window.overrideredirect(TRUE)
 window.resizable(FALSE, FALSE)
+
 window.config(background="#5F5F5F")
+
+welcome_label = Label(text="Starting", bg="#5F5F5F", font=("Trebuchet Ms", 15, "bold"), fg="#FFFFFF")
+welcome_label.place(x=130, y=65)
+
+bg_label = Label(window, image=image, bg="#5F5F5F")
+bg_label.place(x=190, y=330)
+
+progress_label = Label(window, text="Loading...", font=("Trebuchet Ms", 13, "bold"), fg="#FFFFFF", bg="#5F5F5F")
+progress_label.place(x=190, y=330)
+
 
 window.mainloop()
