ca5fae55225889ebe059ac980f46255a8789d7fe
Sierra-Bakery <150887228+Sierra-Bakery@users.noreply.github.com>
2023-12-10 14:17:47 +0800

    Loadscreen makeover revamp and redo, now in sys

--- Graphics.py
+++ Graphics.py
@@ -1,27 +1,14 @@
-from tkinter import *
+import sys
+import time
+import random
+def initiatescreen():
+    print("< Initiating SHADDO.exe >")
+    time.sleep(0.9)
+    for i in range(101):
+        sys.stdout.write('\r')
+        sys.stdout.write("[%-10s] %d%%" % ('='*i, 1*i))
+        sys.stdout.flush()
+        time.sleep((random.randint(1,100)*0.001))
+    print("\n< SHADDO.exe Initiated Sucessfully >")
 
-from PIL import ImageTk, Image
-import main
-window = Tk()
-def Loadscreen():
-    # Making window align to the center
-    height = 430
-    width = 530
-    x = (window.winfo_screenwidth()//2)-(width//2)
-    y = (window.winfo_screenheight()//2)-(height//2)
-    window.geometry('{}x{}+{}+{}'.format(width, height, x, y))
-    #window.overrideredirect(TRUE)
-    window.resizable(FALSE, FALSE)
-    window.config(background="#5F5F5F")
-
-    welcome_label = Label(text="Starting SHADDO Services", bg="#5F5F5F", font=("Trebuchet Ms", 15, "bold"), fg="#FFFFFF")
-    welcome_label.place(x=130, y=65)
-    img = ImageTk.PhotoImage(Image.open("forest.jpg"))
-    bg_label = Label(window, image="loadscreenimg.png", bg="#5F5F5F")
-    bg_label.place(x=190, y=330)
-
-    progress_label = Label(window, text="Loading...", font=("Trebuchet Ms", 13, "bold"), fg="#FFFFFF", bg="#5F5F5F")
-    progress_label.place(x=190, y=330)
-
-
-    window.mainloop()
+e = initiatescreen()
